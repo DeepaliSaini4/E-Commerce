34aec2286d83ef57dce37f6e0a29eb3aeb067b53
Deepali Saini <deepalisa4444@gmail.com>
2025-02-12 15:18:27 +0530

    Added cart button via condtional rendering

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import "./Header.css";
 
 function Header() {
+    const [showcart,setshowCart] = useState(false);
+
+function opencart(){
+    setshowCart(true);
+}
+function closecart(){
+    setshowCart(false);
+}
   return (
     <div className="Header">
       <h1>AURELLE <i>  "grace in every step, elegance in every thread."</i></h1>
+      <div><button className="yellow-button" onClick={opencart}>Cart</button></div>
+      {showcart && (<div className="backdrop" onClick={closecart}>
+        <div className="modal">
+            This is your cart!
+        </div>
+        </div>
+        )} 
     </div>
   );
 }
