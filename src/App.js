5b8620bdc6c101824327f262667bec71c67ef412
Deepali Saini <deepalisa4444@gmail.com>
2025-02-13 00:33:16 +0530

    Updated App.js and Products.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
+import Cart from "./components/Cart/Cart";
+
 
 function App() {
+    const [showCart, setshowCart] = useState(false);
+    const [cartitems, setcartitems] = useState([]);//default value of the state is an empty array
+
+    const openCart = () => setshowCart(true);
+    const closeCart = () =>setshowCart(false);
+
+    const handleaddtocart = (productId, productName, productImage) => {
+      const cartitem = {
+        id: productId,
+        name: productName,
+        image: productImage,
+        quantity: 1,
+      };
+      setcartitems((state) => [...state, cartitem]);
+    };
+
   return (
    <div>
-    <Header />
-    <Products />
+    <Header openCart={openCart} />
+    <Products onaddtocart={handleaddtocart}/>
+    <Cart showCart={showCart} closeCart={closeCart} cartitems={cartitems} />
     </div>
   );
 }
-
 export default App;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,21 +1,26 @@
 import ProductsData from "../../data/products.json";
 import "./Products.css";
 
-function Product({ id, name, image }) {
+function Product({ id, name, image, onaddtocart}) {
   return (
     <div key={id} className="Product">
       <img src={`/assets/${image}`} alt={name} />
       <div className="Product-name">{name}</div>
-      <button className="yellow-button">Add to Cart</button>
+      <button className="yellow-button"
+       onClick={() => onaddtocart(id, name, image)}
+       >
+        Add to Cart
+        </button>
     </div>
   );
 }
 
-function Products() {
+function Products({ onaddtocart }) {
   return (
     <div className="Products-container">
       {ProductsData.map((product) => (
-        <Product key={product.id} id={product.id} name={product.name} image={product.image} />
+        <Product key={product.id} id={product.id} name={product.name} image={product.image}
+        onaddtocart = {(id, name, image) => onaddtocart(id, name, image)} />
       ))}
     </div>
   );
