6e250087c8881d71efdfc55343f893c525f38782
Deepali Saini <deepalisa4444@gmail.com>
2025-02-13 01:39:00 +0530

    Added Cart Quantity Management Functionality

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
 import Cart from "./components/Cart/Cart";
 
-
 function App() {
-    const [showCart, setshowCart] = useState(false);
-    const [cartitems, setcartitems] = useState([]);//default value of the state is an empty array
+  const [showCart, setshowCart] = useState(false);
+  const [cartitems, setcartitems] = useState([]); //default value of the state is an empty array
 
-    const openCart = () => setshowCart(true);
-    const closeCart = () =>setshowCart(false);
+  const openCart = () => setshowCart(true);
+  const closeCart = () => setshowCart(false);
 
-    const handleaddtocart = (productId, productName, productImage) => {
+  const handleaddtocart = (productId, productName, productImage) => {
+    const productincartindex = cartitems.findIndex(
+      (item) => item.id == productId
+    );
+    if (productincartindex == -1) {
       const cartitem = {
         id: productId,
         name: productName,
@@ -19,13 +22,20 @@ function App() {
         quantity: 1,
       };
       setcartitems((state) => [...state, cartitem]);
-    };
+    }else{
+      const updatedcartitmes = [...cartitems];
+      updatedcartitmes[productincartindex].quantity += 1; 
+      setcartitems(updatedcartitmes);
+    }
+  };
+
+
 
   return (
-   <div>
-    <Header openCart={openCart} />
-    <Products onaddtocart={handleaddtocart}/>
-    <Cart showCart={showCart} closeCart={closeCart} cartitems={cartitems} />
+    <div>
+      <Header openCart={openCart} />
+      <Products onaddtocart={handleaddtocart} />
+      <Cart showCart={showCart} closeCart={closeCart} cartitems={cartitems} />
     </div>
   );
 }
