42b776778e184835569bd51916c4049ae92d2fc7
Saini <Saini@Deepalis-MacBook-Air.local>
2025-02-12 14:20:59 +0530

    Initialized project structure with basic file setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>Aurelle ~ <i>"grace in every step, elegance in every thread."</i></h1>
+    </div>
+  );
+}
+
+export default Header;
